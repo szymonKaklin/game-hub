cbc2e95c1b3faaf77fa469c67e555d4bda5bc9c7
szymonKaklin <szymon.kaklin@hotmail.co.uk>
2025-11-20 14:58:05 +0000

    added dynamic page heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,24 @@
+import type { GameQuery } from "@/App";
+import { Heading } from "@chakra-ui/react";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: Props) => {
+  // Games
+  // Action Games
+  // Xbox Games
+  // Xbox Action Games
+  const heading = `${gameQuery.platform?.name || ""} ${
+    gameQuery.genre?.name || ""
+  } Games`;
+
+  return (
+    <Heading as="h1" marginY={5} fontSize="5xl">
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeading;
