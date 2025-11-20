860e955febf60879e9c0b02824a21d03fdceaa83
szymonKaklin <szymon.kaklin@hotmail.co.uk>
2025-11-20 14:35:13 +0000

    built search input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,17 @@
+import { Input, InputGroup } from "@chakra-ui/react";
+import { BsSearch } from "react-icons/bs";
+
+const SearchInput = () => {
+  return (
+    <InputGroup startElement={<BsSearch />}>
+      <Input
+        borderRadius={20}
+        placeholder="Search games..."
+        variant="subtle"
+        bg="bg.subtle"
+      />
+    </InputGroup>
+  );
+};
+
+export default SearchInput;
